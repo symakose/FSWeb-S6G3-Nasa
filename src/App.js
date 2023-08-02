a52b000b1e4c8f4df8ebc03b454dcc170bbe8cc3
Seyma Kose <symakose58@gmail.com>
2023-08-02 22:33:08 +0300

    proje oluşmaya başladı

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React from "react";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { sahteVeri } from "./sahteVeri/sahteVeri";
 
 function App() {
+  const [dateBaslik, setDateBaslik] = useState();
+  const [head, setHead] = useState();
+  const [paragraf, setParagraf] = useState();
+  const [date, setDate] = useState();
+  const [title, setTitle] = useState();
+  const [url, setUrl] = useState();
+  const [expBaslik, setExpBaslik] = useState();
+  const [explanation, setExplanation] = useState();
+  const [count, setCount] = useState(0);
+  const [buton, setButon] = useState("");
+
+  const butonCalistir = () => {
+    setCount(count + 1);
+    setButon("Tomorrow");
+  };
+  useEffect(() => {
+    if (count < sahteVeri.length) {
+      const data = sahteVeri[count];
+      setDateBaslik("Date: ");
+      setTitle(data.title);
+      setDate(data.date);
+      setExplanation(data.explanation);
+      setUrl(data.url);
+    }
+  }, [count]);
+
+  const backgroundUrl = url
+    ? url
+    : "https://wallpaperset.com/w/full/9/5/c/118741.jpg";
   return (
-    <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="App" style={{ backgroundImage: `url(${backgroundUrl})` }}>
+      <h1>Astronomy Picture of the Day</h1>
+      <p>{paragraf}</p>
+      <div>
+        <h2>{title}</h2>
+      </div>
+      <div>
+        <p>
+          <b>{dateBaslik}</b> {date}
+        </p>
+      </div>
+      <div>
+        <p>
+          <b>explanation: </b> {explanation}
+        </p>
+      </div>
+      <button onClick={butonCalistir}>{buton}</button>
     </div>
   );
 }
---
+++ src/sahteVeri/sahteVeri.js
@@ -0,0 +1,68 @@
+export const sahteVeri = [
+  {
+    copyright: "Detlef Hartmann",
+    date: "2023-03-20",
+    explanation:
+      "Are your eyes good enough to see the Crab Nebula expand?  The Crab Nebula is cataloged as M1, the first on Charles Messier's famous list of things which are not comets. In fact, the Crab is now known to be a supernova remnant, an expanding cloud of debris from the explosion of a massive star. The violent birth of the Crab was witnessed by astronomers in the year 1054. Roughly 10 light-years across today, the nebula is still expanding at a rate of over 1,000 kilometers per second. Over the past decade, its expansion has been documented in this stunning time-lapse movie. In each year from 2008 to 2022, an image was produced with the same telescope and camera from a remote observatory in Austria. The sharp, processed frames even reveal the dynamic energetic emission surrounding the rapidly spinning pulsar at the center.  The Crab Nebula lies about 6,500 light-years away toward the constellation of the Bull (Taurus).",
+    media_type: "video",
+    service_version: "v1",
+    thumbnail_url: "https://img.youtube.com/vi/wfzz8FUD4TM/0.jpg",
+    title: "M1: The Expanding Crab Nebula",
+    url: "https://www.youtube.com/embed/wfzz8FUD4TM?rel=0",
+  },
+  {
+    copyright: "Vikas Chander",
+    date: "2023-03-21",
+    explanation:
+      "Can dust be beautiful? Yes, and it can also be useful.  The Taurus molecular cloud has several bright stars, but it is the dark dust that really draws attention.  The pervasive dust has waves and ripples and makes picturesque dust bunnies, but perhaps more importantly, it marks regions where interstellar gas is dense enough to gravitationally contract to form stars. In the image center is a light cloud lit by neighboring stars that is home not only to a famous nebula, but to a very young and massive famous star.  Both the star, T Tauri, and the nebula, Hind's Variable Nebula, are seen to vary dramatically in brightness -- but not necessarily at the same time, adding to the mystery of this intriguing region. T Tauri and similar stars are now generally recognized to be Sun-like stars that are less than a few million years old and so still in the early stages of formation. The featured image spans about four degrees not far from the Pleiades star cluster, while the featured dust field lies about 400 light-years away.",
+    hdurl: "https://apod.nasa.gov/apod/image/2303/TaurusDust_Chander_4096.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Dark Nebulae and Star Formation in Taurus",
+    url: "https://apod.nasa.gov/apod/image/2303/TaurusDust_Chander_1080.jpg",
+  },
+  {
+    copyright: "Abdullah Al-Harbi",
+    date: "2023-03-22",
+    explanation:
+      "How far can you see? The most distant object easily visible to the unaided eye is M31, the great Andromeda Galaxy, over two million light-years away.  Without a telescope, even this immense spiral galaxy appears as an unremarkable, faint, nebulous cloud in the constellation Andromeda.  But a bright white nucleus, dark winding dust lanes, luminous blue spiral arms, and bright red emission nebulas are recorded in this stunning fifteen-hour telescopic digital mosaic of our closest major galactic neighbor.  But how do we know this spiral nebula is really so far away? This question was central to the famous Shapley-Curtis debate of 1920.  M31's great distance was determined in the 1920s by observations that resolved individual stars that changed their brightness in a way that gave up their true distance. The result proved that Andromeda is just like our Milky Way Galaxy -- a conclusion making the rest of the universe much more vast than had ever been previously imagined.",
+    hdurl: "https://apod.nasa.gov/apod/image/2303/M31_Alharbi_4822.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "M31: The Andromeda Galaxy",
+    url: "https://apod.nasa.gov/apod/image/2303/M31_Alharbi_960.jpg",
+  },
+  {
+    copyright: "Roberto Marinoni",
+    date: "2023-03-23",
+    explanation:
+      "A mere 46 million light-years distant, spiral galaxy NGC 2841 can be found in planet Earth's night sky toward the northern constellation of Ursa Major. This sharp image centered on the gorgeous island universe also captures spiky foreground Milky Way stars and more distant background galaxies within the same telescopic field of view. It shows off the bright nucleus of NGC 2841, along with its inclined galactic disk, and faint outer regions.  Dust lanes, small star-forming regions, and young star clusters are embedded in the galaxy's patchy, tightly wound spiral arms. In contrast, many other spirals exhibit broader, sweeping arms with large star-forming regions.  NGC 2841 has a diameter of over 150,000 light-years, making it even larger than our own Milky Way. X-ray images suggest that extreme outflows from giant stars and stellar explosions create plumes of hot gas extending into a halo around NGC 2841.",
+    hdurl: "https://apod.nasa.gov/apod/image/2303/NGC2841_Astrobin.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Spiral Galaxy NGC 2841",
+    url: "https://apod.nasa.gov/apod/image/2303/NGC2841_1024.jpg",
+  },
+  {
+    copyright: "Former darling",
+    date: "2023-03-24",
+    explanation:
+      "Former darling of the northern sky Comet C/2022E3 (ZTF) has faded. During its closest approach to our fair planet in early February Comet ZTF was a mere 2.3 light-minutes distant. Then known as the green comet, this visitor from the remote Oort Cloud is now nearly 13.3 light-minutes away. In this deep image, composed of exposures captured on March 21, the comet still sports a broad, whitish dust tail and greenish tinted coma though. Not far on the sky from Orion's bright star Rigel, Comet ZTF shares the field of view with faint, dusty nebulae and distant background galaxies. The telephoto frame is crowded with Milky Way stars toward the constellation Eridanus. The influence of Jupiter's gravity on the comet's orbit as ZTF headed for the inner solar system, may have set the comet on an outbound journey, never to return.",
+    hdurl: "https://apod.nasa.gov/apod/image/2303/C2022E3_230321.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Outbound Comet ZTF",
+    url: "https://apod.nasa.gov/apod/image/2303/C2022E3_230321_1024.jpg",
+  },
+  {
+    copyright: "Giorgia Hofer",
+    date: "2023-03-25",
+    explanation:
+      "On March 23 early evening skygazers could watch Venus and a young crescent moon, both near the western horizon. On that date Earth's brilliant evening star, faint lunar night side and slender sunlit crescent were captured in this telephoto skyscape posing alongside a church tower from San Pietro di Cadore, Dolomiti, Italy. Of course the subtle lunar illumination is earthshine, earthlight reflected from the Moon's night side. A description of earthshine, in terms of sunlight reflected by Earth's oceans illuminating the Moon's dark surface, was written over 500 years ago by Leonardo da Vinci. On March 24, from some locations the Moon could be seen to occult or pass in front of Venus. Around the planet tonight, a waxing lunar crescent will appear near the Pleiades star cluster.",
+    hdurl: "https://apod.nasa.gov/apod/image/2303/_GHR3094-venerelunafirma.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Venus and the Da Vinci Glow",
+    url: "https://apod.nasa.gov/apod/image/2303/_GHR3094-venerelunafirma800.jpg",
+  },
+];
