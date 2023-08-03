4c755c94952043c3700ceefed153992f86c30bbf
Seyma Kose <symakose58@gmail.com>
2023-08-03 22:22:55 +0300

    styled eklendi.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import { sahteVeri } from "./sahteVeri/sahteVeri";
+import styled from "styled-components";
+
+const AppDiv = styled.div`
+  text-align: center;
+`;
+const AppHead = styled.div`
+  background-color: #b0c4de;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  color: white;
+`;
+const AppButton = styled.button`
+  padding: 10px 5px;
+  border: 2px solid rgb(70, 30, 180);
+  border-radius: 25px;
+  cursor: pointer;
+  font-size: 16px;
+  background-color: #00bfff;
+  color: white;
+`;
 
 function App() {
   const [dateBaslik, setDateBaslik] = useState();
@@ -11,6 +35,7 @@ function App() {
   const [explanation, setExplanation] = useState();
   const [count, setCount] = useState(0);
   const [buton, setButon] = useState("");
+  const [copy, setCopy] = useState();
 
   const butonCalistir = () => {
     setCount(count + 1);
@@ -24,40 +49,52 @@ function App() {
       setDate(data.date);
       setExplanation(data.explanation);
       setUrl(data.url);
+      setCopy(data.copyright);
     }
   }, [count]);
 
   const backgroundUrl = url
     ? url
     : "https://wallpaperset.com/w/full/9/5/c/118741.jpg";
+
   return (
-    <div className="App" style={{ backgroundImage: `url(${backgroundUrl})` }}>
-      <h1>Astronomy Picture of the Day</h1>
-      <p>{paragraf}</p>
-      <div>
-        <h2>{title}</h2>
-      </div>
-      <div>
-        <p>
-          <b>{dateBaslik}</b> {date}
-        </p>
-      </div>
-      <div>
-        <p>
-          <b>Explanation: </b> {explanation}
-        </p>
-      </div>
-      <center>
-        <b> M82: Galaxy with a Supergalactic Wind </b>
-        <a href="https://www.nasa.gov/">NASA</a>,
-        <a href="https://www.esa.int/">ESA</a>,
-        <a href="https://www.nasa.gov/mission_pages/hubble/story/index.html">
-          Hubble
-        </a>
-        ;
-      </center>
-      <button onClick={butonCalistir}>{buton}</button>
-    </div>
+    <AppDiv>
+      <AppHead>
+        <div
+          className="App"
+          style={{ backgroundImage: `url(${backgroundUrl})` }}
+        >
+          <h1>Astronomy Picture of the Previous Day</h1>
+
+          <p>{paragraf}</p>
+          <div>
+            <h2>{title}</h2>
+          </div>
+          <div>
+            <p>
+              <b>{dateBaslik}</b> {date}
+            </p>
+          </div>
+          <div>
+            <p>
+              <b>Explanation: </b> {explanation}
+            </p>
+          </div>
+          <center>
+            <b> M82: Galaxy with a Supergalactic Wind </b>
+            <a href="https://www.nasa.gov/">NASA</a>,
+            <a href="https://www.esa.int/">ESA</a>,
+            <a href="https://www.nasa.gov/mission_pages/hubble/story/index.html">
+              Hubble
+            </a>
+            ;<p>Image Credit & Copyright: {copy}</p>
+          </center>
+          <AppButton>
+            <button onClick={butonCalistir}>{buton}</button>
+          </AppButton>
+        </div>
+      </AppHead>
+    </AppDiv>
   );
 }
 
---
+++ src/pictures.js
@@ -0,0 +1,18 @@
+// import React, { useState } from "react";
+// import { sahteVeri } from "./sahteVeri/sahteVeri";
+
+// function Pictures() {
+//   const [picture, setPicture] = useState(sahteVeri);
+
+//   return (
+//     <div className="Main-Container">
+//       {picture.map((x) => {
+//         if (!x.hdurl) {
+//           picture.hdurl = picture["hdurl"];
+//         }
+//       })}
+//     </div>
+//   );
+// }
+
+// export default Pictures;
